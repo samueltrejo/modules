dac0cce7116ac0962d0d77f28415fc09044d89f5
Samuel Trejo <samuel.trejo@s.tcatnashville.edu>
2019-04-08 21:26:12 -0500

    store component prints to page

---
+++ javascripts/helpers/book.js
@@ -0,0 +1,11 @@
+const bookInfo = {
+  price: 24.99,
+  title: 'Beautiful Creatures',
+  image: './assets/book.jpg'
+}
+
+const getBook = () => {
+  return bookInfo;
+};
+
+export default {getBook};
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,7 @@
-console.log('whack dope')
+import store from './components/store.js'
+
+const init = () => {
+  store.makeStore();
+};
+
+init();
